2d3ba8cfa330c4e382ab8a7855ac3cf92fa4d56b
Anes72 <loubaranes333@gmail.com>
2024-11-29 12:55:00 +0100

    Update Paiment.java

--- backend/Paiment.java
+++ backend/Paiment.java
@@ -1,98 +1,102 @@
 
-import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
-/**
- * 
- */
-public class Paiment {
+public class Paiement {
 
-    /**
-     * Default constructor
-     */
-    public Paiment() {
-    }
+    Scanner scanner = new Scanner(System.in);
 
-    /**
-     * 
-     */
     private int id_paiement;
 
-    /**
-     * 
-     */
     private int montant;
 
-    /**
-     * 
-     */
     private Date date_paiment;
 
-    /**
-     * 
-     */
-    private String mode_paiment;
+    Paiement(int id_paiement, int montant, Date date_paiment) {
+        this.id_paiement = id_paiement;
+        this.montant = montant;
+        this.date_paiment = date_paiment;
+    }
 
-    /**
-     * @return
-     */
     public int getIdPaiement() {
-        // TODO implement here
-        return 0;
+
+        return id_paiement;
     }
 
-    /**
-     * @param int newId_paiement
-     * @return
-     */
     public void setIdPaiement(int newId_paiement) {
-        // TODO implement here
+        id_paiement = newId_paiement;
     }
 
-    /**
-     * @return
-     */
     public int getMontant() {
-        // TODO implement here
-        return 0;
+
+        return montant;
     }
 
-    /**
-     * @param int newMontant
-     * @return
-     */
     public void setMontant(int newMontant) {
-        // TODO implement here
+        montant = newMontant;
     }
 
-    /**
-     * @return
-     */
     public Date getDatePaiement() {
-        // TODO implement here
-        return null;
+
+        return date_paiment;
     }
 
-    /**
-     * @param Date newDate_paiment
-     * @return
-     */
     public void setDatePaiement(Date newDate_paiment) {
-        // TODO implement here
+        date_paiment = newDate_paiment;
     }
 
-    /**
-     * @return
-     */
-    public void effectuerPaiement() {
-        // TODO implement here
+    public void effectuerPaiement(int argentDonné) {
+        if (argentDonné > this.montant) {
+            int x = argentDonné - this.montant;
+            System.out.println("Paiement du montant effectué, voici l'argent a rendre:" + x);
+        }
+        if (argentDonné < this.montant) {
+            int x = this.montant - argentDonné;
+            System.out.println("il manque encore" + x);
+            System.out.println("Donne l'argent ajouté par le client:");
+            int y = Integer.parseInt(scanner.nextLine());
+            argentDonné = argentDonné + y;
+            effectuerPaiement(argentDonné);
+        }
+        if (argentDonné == this.montant) {
+            System.out.println("Paiement du montant effectué");
+        }
     }
 
-    /**
-     * @return
-     */
-    public void effectuerPaiementMaintenance() {
-        // TODO implement here
+    public void effectuerPaiementMaintenance(int argentDonné, Retour retour) {
+        if (argentDonné > retour.getFraisSupplementaires()) {
+            int x = argentDonné - retour.getFraisSupplementaires();
+            System.out.println("Paiement de la maintenance effectué, voici l'argent a rendre:" + x);
+        }
+        if (argentDonné < retour.getFraisSupplementaires()) {
+            int x = retour.getFraisSupplementaires() - argentDonné;
+            System.out.println("il manque encore" + x);
+            System.out.println("Donne l'argent ajouté par le client:");
+            int y = Integer.parseInt(scanner.nextLine());
+            argentDonné = argentDonné + y;
+            effectuerPaiement(argentDonné);
+        }
+        if (argentDonné == retour.getFraisSupplementaires()) {
+            System.out.println("Paiement de la maintenance effectué");
+        }
+    }
+
+    public void saveToDatabase(Connection conn, int id_paiement, int montant, Date date_paiment) throws SQLException {// good
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String formattedDate = sdf.format(date_paiment); // Convert Date to string in 'YYYY-MM-DD' format
+
+        String creatSQL = "INSERT INTO Retour(id_paiement, montant, date_paiment) VALUES (?,?,?,?,?)";
+        PreparedStatement stmt = conn.prepareStatement(creatSQL);
+        stmt.setInt(1, id_paiement);
+        stmt.setInt(2, montant);
+        stmt.setString(3, formattedDate);
+        stmt.executeUpdate();
+        System.out.println("Données insérées avec succès !");
     }
 
 }
+
